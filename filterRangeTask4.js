2929ddd9c8e9d95c0a6de446813dfa57951445b0
taras2480 <aviator2480@gmail.com>
2020-01-05 01:02:30 +0200

    init js hw3

---
+++ filterRangeTask4.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function filterRange(array, a, b) {
+
+	console.log(array.slice(a, b));
+}
+	
+	let products = [ 'products1', 'products1', 'products1', 'products1',
+		'products190', 'products2345', 'products2323', 'products1333',
+		'products16778', 'products187' ];
+	
+	
+	filterRange(products, 4, 7);
+	
+	
+	
+	
+
---
+++ productsTask1.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let products = [ 'products1', 'products1', 'products1', 'products1',
+		'products190', 'products2345', 'products2323', 'products1333',
+		'products16778', 'products187' ];
+
+let lastElements = products[products.length - 1];
+console.log(lastElements);
