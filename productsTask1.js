a3bd07c0d13fba3bd2f05664409e5ff74ee2658c
taras2480 <aviator2480@gmail.com>
2020-01-09 23:05:44 +0200

    init lesson-04 JS

--- productsTask1.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-let products = [ 'products1', 'products1', 'products1', 'products1',
-		'products190', 'products2345', 'products2323', 'products1333',
-		'products16778', 'products187' ];
-
-let lastElements = products[products.length - 1];
-console.log(lastElements);
